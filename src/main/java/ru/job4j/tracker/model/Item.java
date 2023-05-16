0d04a96875565f4fdf2a83e50aa0df916d9d6453
Paramonov Dmitriy <paramonovdn@mail.ru>
2023-05-16 19:23:06 +0300

    job4j_tracker

---
+++ src/main/java/ru/job4j/tracker/model/Item.java
@@ -0,0 +1,57 @@
+package ru.job4j.tracker.model;
+
+import java.util.Objects;
+
+public class Item {
+
+    private int id;
+
+    private String name;
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("id: %s, name: %s", id, name);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Item item = (Item) o;
+        return Objects.equals(id, item.id) && Objects.equals(name, item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
