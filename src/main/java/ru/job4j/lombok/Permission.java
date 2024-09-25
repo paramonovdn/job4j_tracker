2d2e6d419d6d6c1f8d5060ec515400664339a1e4
paramonovdn@mail.ru <paramonovdn@mail.ru>
2024-09-25 11:31:41 +0300

    3. @Builder [#504890]

---
+++ src/main/java/ru/job4j/lombok/Permission.java
@@ -0,0 +1,27 @@
+package ru.job4j.lombok;
+
+import java.util.List;
+import lombok.*;
+
+@Builder(builderMethodName = "of")
+@ToString
+public class Permission {
+    private int id;
+    private String name;
+    @Singular("accessBy")
+    private List<String> rules;
+
+    public static class PermissionUsage {
+        public static void main(String[] args) {
+            var permission = Permission.of()
+                    .id(1)
+                    .name("John")
+                    .accessBy("create")
+                    .accessBy("update")
+                    .accessBy("read")
+                    .accessBy("delete")
+                    .build();
+            System.out.println(permission);
+        }
+    }
+}
